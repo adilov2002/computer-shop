cebc6c36f18620d0ec51303b0ecaadaba6c6c767
Adilov Zhanniyet <joni.2002.08@gmail.com>
2022-09-20 11:10:50 +0600

    Initial commit

---
+++ src/main/java/kz/iitu/computershop/ComputerShopApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.computershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/iitu/computershop/ComputerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.computershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
